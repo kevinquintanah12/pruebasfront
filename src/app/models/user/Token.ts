3a953671bdb8302f0b89fb05c0d58bf556018abd
kevinquintanah12 <kevinherreraq12@gmail.com>
2024-12-02 07:50:19 -0600

    2

--- src/app/models/user/Token.ts
+++ src/app/models/user/Token.ts
@@ -1,5 +1,11 @@
 export class Token {
-    id: String = "";
-    user: String = "";
-    token: String = ""
+    id: string;
+    user: string;
+    token: string;
+
+    constructor(id: string = "defaultId", user: string = "defaultUser", token: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtldmluZW1tYW51ZWwiLCJleHAiOjE3MzMxMTY1ODIsIm9yaWdJYXQiOjE3MzMxMTYyODJ9.kxG8Vc2hA06kVQVNcwLss-hRwmyp1QCuKM2i69qUhpA") {
+        this.id = id;
+        this.user = user;
+        this.token = token;
+    }
 }
